4761d1a20a9ec6b3fca0b3abf9e69b0472e881a1
Jeff Levensailor <jeff@levensailor.com>
2018-05-15 00:04:05 -0700

    test

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var request = require("request");
+var http = require('http'),
+    path = require('path'),
+    os = require('os'),
+    fs = require('fs');
 
+var request = require("request");
 var options = { method: 'POST',
     url: 'https://50.254.224.200:8443/logcollectionservice/services/DimeGetFileService',
     rejectUnauthorized: false,
     headers: 
-    { 'Cache-Control': 'no-cache',
-    'Connection': 'Keep-Alive',
+    {
     Authorization: 'Basic YWRtaW5pc3RyYXRvcjpkdXJoYW1t',
-    SOAPAction: 'http://schemas.cisco.com/ast/soap/action/#LogCollectionPort#GetOneFile' },
+    SOAPAction: 'http://schemas.cisco.com/ast/soap/action/#LogCollectionPort#GetOneFile' 
+    },
+    encoding: 'binary',
+    gzip: true,
     body: 
     '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://schemas.cisco.com/ast/soap/">'+
     '<soapenv:Header/>'+
@@ -19,12 +25,29 @@ var options = { method: 'POST',
     '</soapenv:Envelope>' 
     };
 
-request(options, function (error, response, body) {
+request(options, function (error, res, body) {
   if (error) throw new Error(error);
 
-  console.log(body);
+  //   // let boundaryRegex = new RegExp(/boundary=(\S+)/g);
+  //   // const header = JSON.stringify(res.headers);
+  //   // const boundary = '--' + boundaryRegex.exec(header)[1]; // We obtain the boundary here
+  //   // rawFileContent = rawBody.split(boundary);
 
-//body contains multiple parts. how to get attachment? 🤷🏼‍♂️
+  rawBody = Buffer.from(res.body);
+  start = 0;
+  end = rawBody.length;
+  for (x=0; x<12; x++){
+    start = rawBody.indexOf('\r\n',start);
+    start += 2;
+  }
+  for (z=rawBody.length; z>rawBody.length-2; z--){
+    end = rawBody.indexOf('\r\n',end);
+    end -= 2;
+  }
+  console.log('length: '+rawBody.length);
+  console.log('end: '+end);
+  rawFileContent = rawBody.slice(start).slice(-(rawBody.length), -444);
 
+  fs.writeFile('syslog.txt.gz', rawFileContent, function(err){});
 
 });
